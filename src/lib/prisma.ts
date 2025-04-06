a32658ab95bf852e6dbf15699ce633c0ad80e3ec
Your Name <rafikhafr1@gmail.com>
2025-04-06 17:42:11 +0100

    fix prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || (process.env.NODE_ENV !== 'production' && typeof window === 'undefined' ? new PrismaClient() : null);
 
 export default prisma
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production' && typeof window === 'undefined') globalForPrisma.prisma = prisma;
