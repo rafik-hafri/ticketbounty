b15762152d9595eb09938dddfdc78017d91c4fb5
Your Name <rafikhafr1@gmail.com>
2025-05-18 15:31:42 +0100

    create comment

--- src/features/comment/components/comment.tsx
+++ src/features/comment/components/comment.tsx
@@ -1,4 +1,6 @@
+import CardCompact from "@/components/card-compact"
 import { getComments } from "../queries/get-comments"
+import CommentCreateForm from "./comment-create-form"
 import CommentItem from "./comment-item"
 
 type CommentsProps = {
@@ -8,11 +10,18 @@ type CommentsProps = {
 async function Comments({ticketId}: CommentsProps) {
     const comments  = await getComments(ticketId)
   return (
+    <>
+    <CardCompact 
+    title="Create Comment" 
+    description="A new comment will be created"
+    content={<CommentCreateForm ticketId={ticketId}/>}
+    />
     <div className="flex flex-col gap-y-2 ml-8">
         {comments.map((comment)=> (
             <CommentItem key={comment.id} comment={comment}/>
         ))}
     </div>
+    </>
   )
 }
 
