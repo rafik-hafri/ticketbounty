ae2957dab0256aa8145ce8eba5dd7129adbd65e6
Your Name <rafikhafr1@gmail.com>
2025-05-28 19:37:03 +0100

    comments infinite scrolling

--- src/features/comment/components/comment.tsx
+++ src/features/comment/components/comment.tsx
@@ -1,15 +1,17 @@
 "use client"
-import { useState } from "react"
+// import { useState } from "react"
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query"
+import { useEffect } from "react"
+import {useInView} from "react-intersection-observer"
 import CardCompact from "@/components/card-compact"
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 import { CommentWithMetadata } from '@/features/comment/types'
 import { PaginatedData } from "@/types/pagination"
 import { getComments } from "../queries/get-comments"
 import CommentCreateForm from "./comment-create-form"
 import CommentDeleteButton from "./comment-delete-button"
 import CommentItem from "./comment-item"
-import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query"
-import { metadata } from "@/app/layout"
+// import { metadata } from "@/app/layout"
 
 
 type CommentsProps = {
@@ -21,7 +23,7 @@ type CommentsProps = {
     // const [comments, setComments] = useState(paginatedComments.list)
     // const [metadata, setMetadata] = useState(paginatedComments.metadata)
     const queryKey = ["comments", ticketId]
-    const {data, fetchNextPage, hasNextPage, isFetchingNextPage, refetch} = useInfiniteQuery({
+    const {data, fetchNextPage, hasNextPage, isFetchingNextPage} = useInfiniteQuery({
         queryKey,
         queryFn: ({pageParam}) => getComments(ticketId, pageParam),
         initialPageParam: undefined as (string | undefined),
@@ -40,53 +42,82 @@ type CommentsProps = {
     const comments = data.pages.map(page => page.list).flat() 
     const queryClient = useQueryClient()
 
-    const handleMore = () => {
+    // const handleMore = () => {
 
-    //  const morePaginatedComments = await getComments(ticketId, metadata.cursor )
-    //  const moreComments = morePaginatedComments.list
-    //  setComments([...comments, ...moreComments])
-    //  setMetadata(morePaginatedComments.metadata)
-    fetchNextPage()
+    // //  const morePaginatedComments = await getComments(ticketId, metadata.cursor )
+    // //  const moreComments = morePaginatedComments.list
+    // //  setComments([...comments, ...moreComments])
+    // //  setMetadata(morePaginatedComments.metadata)
+    // fetchNextPage()
 
-    }
+    // }
+    //  const handleDelteComment = (_id: string) => {
+    //     // console.log("deleted")
+    //     // setComments((prevComments) => prevComments.filter((comment) => comment.id !== id))
+    //     // refetch()
+    //  }
 
-    const handleDelteComment = (id: string) => {
+    const handleDelteComment = () => {
         // console.log("deleted")
         // setComments((prevComments) => prevComments.filter((comment) => comment.id !== id))
         // refetch()
         queryClient.invalidateQueries({queryKey})
     }
+    
 
-    const handleCreateComment = (comment: CommentWithMetadata | undefined)=> {
+    //  const handleCreateComment = (comment: CommentWithMetadata | undefined)=> {
+    //     // if(!comment) return
+    //     // setComments((prevComments) => [comment, ...prevComments])
+    //     // refetch()
+    // }
+    const handleCreateComment = ()=> {
         // if(!comment) return
         // setComments((prevComments) => [comment, ...prevComments])
         // refetch()
         queryClient.invalidateQueries({queryKey})
 
     }
+
+    const {ref, inView} = useInView()
+    useEffect(()=> {
+        if(inView && hasNextPage && !isFetchingNextPage){
+            fetchNextPage()
+        }
+    }, [fetchNextPage, hasNextPage, inView, isFetchingNextPage])
   return (
     <>
-    <CardCompact 
-    title="Create Comment" 
-    description="A new comment will be created"
-    content={<CommentCreateForm ticketId={ticketId} onCreateComment={handleCreateComment}/>}
-    />
-    <div className="flex flex-col gap-y-2 ml-8">
-        {comments.map((comment)=> (
-            <CommentItem 
-            key={comment.id}
-            comment={comment}
-            buttons={[
-                ...(comment.isOwner ? [<CommentDeleteButton key="0" id={comment.id} onDeleteComment={handleDelteComment}/>]:[])
-            ]}            
-            />
-        ))}
-    </div>
-    <div className="flex flex-col justify-center ml-8">
-        {/* {metadata.hasNextPage && (<Button variant="ghost" onClick={handleMore}>More</Button>)} */}
-        {hasNextPage && (<Button variant="ghost" onClick={handleMore} disabled={isFetchingNextPage}>More</Button>)}
-
-    </div>
+        <CardCompact 
+        title="Create Comment" 
+        description="A new comment will be created"
+        content={<CommentCreateForm ticketId={ticketId} onCreateComment={handleCreateComment}/>}
+        />
+            <div className="flex flex-col gap-y-2 ml-8">
+                {comments.map((comment)=> (
+                    <CommentItem 
+                    key={comment.id}
+                    comment={comment}
+                    buttons={[
+                        ...(comment.isOwner ? [<CommentDeleteButton key="0" id={comment.id} onDeleteComment={handleDelteComment}/>]:[])
+                    ]}            
+                    />
+                ))}
+            </div>
+            {/* 
+            <div className="flex flex-col justify-center ml-8">
+                {hasNextPage && (
+                    <Button variant="ghost" onClick={handleMore} disabled={isFetchingNextPage}>
+                        More
+                    </Button>
+                )}
+            </div> 
+            */}
+            <div ref={ref}>
+                {
+                    !hasNextPage && (
+                        <p className="text-right text-xs italic">No more comments</p>
+                    )
+                }
+            </div>
     </>
   )
 }
